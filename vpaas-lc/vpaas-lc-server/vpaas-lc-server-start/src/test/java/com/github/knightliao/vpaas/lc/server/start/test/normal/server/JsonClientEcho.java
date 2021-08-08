2f054f0e9514773432f50deccf960ec2fe1e6dcb
knightliao <knightliao@gmail.com>
2021-08-08 17:36:45 +0800

    re

---
+++ vpaas-lc/vpaas-lc-server/vpaas-lc-server-start/src/test/java/com/github/knightliao/vpaas/lc/server/start/test/normal/server/JsonClientEcho.java
@@ -0,0 +1,16 @@
+package com.github.knightliao.vpaas.lc.server.start.test.normal.server;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author knightliao
+ * @date 2021/8/8 17:25
+ */
+@Slf4j
+public class JsonClientEcho {
+
+    public static  void main(String[] args) throws Exception{
+
+
+    }
+}
