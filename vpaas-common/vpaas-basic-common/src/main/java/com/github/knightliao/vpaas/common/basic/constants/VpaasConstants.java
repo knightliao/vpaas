32eddea152690c87edaf26e486518c3175f4d86a
knightliao <knightliao@gmail.com>
2021-08-12 11:44:16 +0800

    re

--- vpaas-common/vpaas-basic-common/src/main/java/com/github/knightliao/vpaas/common/basic/constants/VpaasConstants.java
+++ vpaas-common/vpaas-basic-common/src/main/java/com/github/knightliao/vpaas/common/basic/constants/VpaasConstants.java
@@ -10,4 +10,7 @@ public class VpaasConstants {
     public static final int DEFAULT_ERROR_VALUE_INT = -1;
     public static final long DEFAULT_ERROR_VALUE_LONG = -1;
     public static final String DEFAULT_ERROR_VALUE_STRING = "";
+
+    //
+    public static final long DEFAULT_ERROR_UID = -1;
 }
