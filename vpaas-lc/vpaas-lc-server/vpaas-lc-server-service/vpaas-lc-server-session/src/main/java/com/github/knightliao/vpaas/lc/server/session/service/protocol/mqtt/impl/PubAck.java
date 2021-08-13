05c6a30f5d28f39c11f2c2c5de8616f4e5245965
knightliao <knightliao@gmail.com>
2021-08-13 14:41:07 +0800

    预添加各种语义

---
+++ vpaas-lc/vpaas-lc-server/vpaas-lc-server-service/vpaas-lc-server-session/src/main/java/com/github/knightliao/vpaas/lc/server/session/service/protocol/mqtt/impl/PubAck.java
@@ -0,0 +1,35 @@
+package com.github.knightliao.vpaas.lc.server.session.service.protocol.mqtt.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.github.knightliao.vpaas.lc.server.session.service.protocol.ProtocolProcessor;
+
+import io.netty.channel.Channel;
+
+/**
+ * @author knightliao
+ * @email knightliao@gmail.com
+ * @date 2021/8/13 14:36
+ */
+@Service(value = "PubAck")
+public class PubAck implements ProtocolProcessor {
+    @Override
+    public void doPre(Channel channel, Object msg) {
+
+    }
+
+    @Override
+    public void doAfter(Channel channel, Object msg) {
+
+    }
+
+    @Override
+    public void process(Channel channel, Object msg) {
+
+    }
+
+    @Override
+    public boolean canGo(Object msg) {
+        return false;
+    }
+}
